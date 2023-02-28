5436dd4f9c74cbcfaa68a2f776e99489dbaa51b6
mohitleo03 <sushilkumarmalikfootwear@gmail.com>
2023-02-28 23:15:17 +0530

    config lists

--- controller/config_lists.js
+++ controller/config_lists.js
@@ -4,8 +4,8 @@ const ConfigListsOperations = require("../db/services/config_lists");
 const configListsController = {
     async getConfigLists(request,response){
         let configLists = await ConfigListsOperations.getConfigLists();
-        if(configLists){
-            response.status(SUCCESS).json({configLists:configLists});           
+        if(configLists){console.log(configLists)
+            response.status(SUCCESS).json(configLists);           
         }
         else{
             response.status(NOT_FOUND).json({message:messageBundle['unsuccessful']});
--- db/services/config_lists.js
+++ db/services/config_lists.js
@@ -1,7 +1,7 @@
 const ConfigListsModel = require("../models/config_lists");
 module.exports = {
   async getConfigLists() {
-    let congifLists = await ConfigListsModel.find();
+    let congifLists = await ConfigListsModel.findOne({},{categoryList:1,sizeRangeList:1,_id:0});
     return congifLists;
   },
 };
