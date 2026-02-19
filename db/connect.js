0e46a636c02f9ca90561ccf8a8415733b2535708
Mohit Malik <126309156+SushilKumarMalikFootwear@users.noreply.github.com>
2026-02-19 19:21:55 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -2,12 +2,5 @@ const mongoose = require('mongoose');
 const dbOptions = {
     maxPoolSize:5
 }
-mongoose.connect('mongodb+srv://sushilkumarmalikfootwear:sushilkumarmalikfootwear@sushilkumarmalikfootwea.4y2qtto.mongodb.net/',dbOptions,err=>{
-    if(err){
-        console.log("database connection failed",err);
-    }
-    else{
-        console.log("Database connection created sucessfully...")
-    }
-});
+mongoose.connect('mongodb+srv://sushilkumarmalikfootwear:sushilkumarmalikfootwear@sushilkumarmalikfootwea.4y2qtto.mongodb.net/',dbOptions);
 module.exports = mongoose;
